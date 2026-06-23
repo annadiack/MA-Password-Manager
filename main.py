32d142b05784d0bad9d7c0842fdcf235726c687f
annadiack <46572304+annadiack@users.noreply.github.com>
2026-06-23 17:56:07 +0200

    Initial: Haupteinstiegspunkt

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+MA Password Manager - Haupteinstiegspunkt
+
+Ein sicherer Passwortmanager mit Konsolenschnittstelle für die Masterarbeit.
+Ermöglicht das Speichern, Abrufen und Generieren von Passwörtern mit AES-256 Verschlüsselung.
+
+Verwendung:
+    python main.py
+"""
+
+from password_manager.ui import main
+
+if __name__ == "__main__":
+    main()
